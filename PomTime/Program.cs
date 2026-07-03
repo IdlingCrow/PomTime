6dfc71452d84a5a23eca5768e3ab86ce2007d71d
IdlingCrow <hoangkienle37@gmail.com>
2026-07-03 11:26:48 -0700

    created a hello world

---
+++ PomTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Timers;
+
+namespace prototype
+{
+    public class pomPrototype
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Write("What is your name?: ");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + userName + "!");
+        }
+    }
+}
+
+
