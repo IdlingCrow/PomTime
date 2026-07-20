98f5b8a9be11fccb1522b6847c5d67362374d826
IdlingCrow <hoangkienle37@gmail.com>
2026-07-20 16:45:41 -0700

    created the first test

--- PomTime.Tests/Test1.cs
+++ PomTime.Tests/Test1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading;
+using PomTimeApp;
 
 namespace PomTime.Tests;
 
@@ -9,7 +10,17 @@ public sealed class Test1
 {
 
     [TestMethod]
-    public void TestMethod1()
+    public void correctInput()
     {
+        StartingUI newUi = new StartingUI();
+        int breakMinutes = newUi.getBreakMinutes();
+        int breakSeconds = newUi.getBreakSeconds();
+        int workMinutes = newUi.getWorkMinutes();
+        int workSeconds = newUi.getWorkSeconds();
+        
+        Assert.AreEqual(0, breakMinutes, $"Number that was recived was: {breakMinutes}");
+        Assert.AreEqual(0, breakSeconds, $"Number that was recived was: {breakSeconds}");
+        Assert.AreEqual(0, workMinutes, $"Number that was recived was: {workMinutes}");
+        Assert.AreEqual(0, workSeconds, $"Number that was recived was: {workSeconds}");
     }
 }
