ee2cc774cc1d364cfa9ed15b840dbf13d5b16ae8
IdlingCrow <hoangkienle37@gmail.com>
2026-07-20 19:22:49 -0700

    added the model test

--- PomTime.Tests/modelTest.cs
+++ PomTime.Tests/modelTest.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using PomTimeApp;
 
 namespace PomTime.Tests;
@@ -5,5 +6,17 @@ namespace PomTime.Tests;
 [TestClass]
 public sealed class modelTest
 {
-    
+    [TestMethod]
+    public void timerTestDataRecording()
+    {
+        TimeModel timer = new TimeModel(60, 60, 50, 50);
+
+        Assert.AreEqual(3050, timer.getBreakTime(), $"break timer is suppose to be 3050 is but is {timer.getBreakTime()}");
+        Assert.AreEqual(3660, timer.getWorkTime(), $"work timer is suppose to be 3660 is but is {timer.getWorkTime()}");
+
+        timer.changeTime(0, 50, 0, 60);
+
+        Assert.AreEqual(60, timer.getBreakTime(), $"change break timer is suppose to be 50 is but is {timer.getBreakTime()}");
+        Assert.AreEqual(50, timer.getWorkTime(), $"change work timer is suppose to be 60 is but is {timer.getWorkTime()}");
+    }   
 }
