1205fc581a1201c247970363e812cc93f435c190
IdlingCrow <hoangkienle37@gmail.com>
2026-08-12 17:35:54 -0700

    added some misc file

--- PomTime.Tests/obj/Debug/net10.0-windows/PomTime.Tests.AssemblyInfo.cs
+++ PomTime.Tests/obj/Debug/net10.0-windows/PomTime.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PomTime.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+913a694babe921833f8592e338d03df076cc263c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7b9d672f21be72881d414bc31f33b1b624ae15a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PomTime.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PomTime.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
